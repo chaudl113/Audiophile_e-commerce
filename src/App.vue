46b45ff208355b1372773f274f3e7bd7944df0a2
chaudl113 <tnlong1214@gmail.com>
2021-06-16 02:02:41 +0700

    up

--- src/App.vue
+++ src/App.vue
@@ -3,45 +3,13 @@
 </template>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@300;700;800&display=swap");
 
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: "Poppins", sans-serif;
-  background-color: #141625;
-}
-
-button,
-.button {
-  cursor: pointer;
-  padding: 16px 24px;
-  border-radius: 30px;
-  border: none;
-  font-size: 12px;
-  margin-right: 8px;
-  color: #fff;
-}
-
-.dark-purple {
-  background-color: #252945;
-}
-
-.red {
-  background-color: #ec5757;
-}
-
-.purple {
-  background-color: #7c5dfa;
-}
-
-.green {
-  background-color: #33d69f;
-}
-
-.orange {
-  background-color: #ff8f00;
+  font-family: "Manrope", sans-serif;
 }
 
 // utility classes
@@ -73,41 +41,12 @@ button,
 // Status Button Styling
 
 .status-button {
-  &::before {
-    content: "";
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    margin-right: 8px;
-  }
-  font-size: 12px;
+  font-size: 0.875rem;
   margin-right: 30px;
   align-items: center;
-  padding: 8px 30px;
-  border-radius: 10px;
-}
-
-.paid {
-  &::before {
-    background-color: #33d69f;
-  }
-  color: #33d69f;
-  background-color: rgba(51, 214, 160, 0.1);
-}
-
-.pending {
-  &::before {
-    background-color: #ff8f00;
-  }
-  color: #ff8f00;
-  background-color: rgba(255, 145, 0, 0.1);
-}
-
-.draft {
-  &::before {
-    background-color: #dfe3fa;
-  }
-  color: #dfe3fa;
-  background-color: rgba(223, 227, 250, 0.1);
+  font-weight: 700;
+  padding: 0.9rem 1.665rem;
+  border: none;
+  text-transform: uppercase;
 }
 </style>
